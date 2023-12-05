3c57b370aacab0ceba29700418f8277b9b5bb9b4
Kivthor <50101494+Kivthor@users.noreply.github.com>
2023-12-05 17:28:26 +0700

    completed third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let newString = '';
+  for (let i = 1; i <= string.length; i++) {
+    newString += string.substr(-i, 1);
+    // console.log(`${i} LOOP ITERATION --- newString = '${newString}'`);
+  }
+  return newString;
 };
 
 // Do not edit below this line
