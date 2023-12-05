7237925f61ee633c890bb8edd0d6439537b29d68
Kivthor <50101494+Kivthor@users.noreply.github.com>
2023-12-05 16:10:19 +0700

    completed second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  // определяем новую переменную, которую будет выводить функция 
+  let newString = '';
+  // если требуемое число повторений строки меньше нуля то функция возвратит "ERROR"
+  if (num < 0) {
+    newString = 'ERROR';
+    return newString;
+  }
 
+  for (let i = 0; i < num; i++) {
+    newString = string; // => newString = 'hey'
+    string += string.slice(0, 3); // => string = 'hey' + 'hey; => 'heyhey'
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
