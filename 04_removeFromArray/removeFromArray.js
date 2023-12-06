70fa87d29e8b76946e01e71174ccd31bbe9d9208
Kivthor <50101494+Kivthor@users.noreply.github.com>
2023-12-07 03:05:46 +0700

    implemented my own solution in removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function(array, ...theArgs) {
-  return newArray = array.filter((elements) => !theArgs.includes(elements));
+  
+  for (let i = 0; i < array.length; i++) {
+    for (let k = 0; k < theArgs.length; k++) {
+      if (array[i] === theArgs[k]) {
+        array.splice(array.indexOf(array[i]), 1);
+        i--;
+        break;
+      }
+    }
+  }
+
+  return array;
+
+  // return newArray = array.filter((elements) => !theArgs.includes(elements));
 };
 
 // Do not edit below this line
