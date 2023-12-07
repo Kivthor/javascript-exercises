387014262a433109367555bf91f58165757f326f
Kivthor <50101494+Kivthor@users.noreply.github.com>
2023-12-07 11:55:10 +0700

    modified script so that it exepts string of any length;

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,17 @@
 const repeatString = function(string, num) {
-  // определяем новую переменную, которую будет выводить функция 
-  let newString = '';
-  // если требуемое число повторений строки меньше нуля то функция возвратит "ERROR"
   if (num < 0) {
-    newString = 'ERROR';
-    return newString;
+   return 'ERROR';
   }
+
+  const inputStringLength = string.length;
+  let newString = '';
 
   for (let i = 0; i < num; i++) {
     newString = string; // => newString = 'hey'
-    string += string.slice(0, 3); // => string = 'hey' + 'hey; => 'heyhey'
+    string += string.slice(0, inputStringLength); // => string = 'hey' + 'hey; => 'heyhey'
+    console.log(`input string length = ${inputStringLength}`);
   }
-
+  console.log('---------END OF THE LOOP--------');
   return newString;
 };
 
