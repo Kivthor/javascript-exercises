87324ddf0667425b745adcf9b48aa0efa0dc5d14
Kivthor <50101494+Kivthor@users.noreply.github.com>
2023-12-07 21:23:54 +0700

    added comments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,20 +2,10 @@ const leapYears = function (year) {
   if (year % 4 === 0) {
     if (year % 100 === 0) {
       if (year % 400 === 0) {
-        return true;
-      } else return false;
-    } else return true;
-  } else return false;
-
-  // if ((year % 4 === 0) && (year % 100 === 0) && (year % 400 === 0)) {
-  //   return true;
-  // } else if (year % 4 !== 0) {
-  //   return false;
-  // } else if (year % 100 !== 0) {
-  //   return true;
-  // } else if (year % 400 !== 0) {
-  //   return false;
-  // }
+        return true; // если год делится на 4, 100 и 400, значит год Високосный
+      } else return false; // если год делится на 4 и 100, но НЕ делится на 400, то год НЕ Високосный
+    } else return true; // если год делится на 4, но НЕ делится на 100, то год Високосный
+  } else return false; // если год НЕ делится на 4, то год НЕ Високосный
 };
 
 // Do not edit below this line
