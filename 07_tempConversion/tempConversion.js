9b2cce8814be7b59f2b09b31ab259140759c97ee
Kivthor <50101494+Kivthor@users.noreply.github.com>
2023-12-08 00:51:29 +0700

    completed seventh exercise;

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convTemp = (temp - 32) * (5 / 9);
+  console.log(convTemp);
+  convTemp = Math.round(convTemp * 10) / 10;
+  console.log(convTemp);
+  return convTemp;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  return Math.round((temp2 * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
